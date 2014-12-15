715d7028064e769d3e6d3319424b46bd9501cd59
David Worthington <david.worthington@outlook.com>
2014-12-15 11:16:26 +0100

    more jasmine tests

---
+++ TimeTracker.Web/Scripts/app/routeConfigSpec.js
@@ -0,0 +1,61 @@
+﻿///<reference path="~/Scripts/jasmine/jasmine.js"/>
+///<reference path="~/Scripts/angular.js"/>
+///<reference path="~/Scripts/angular-route.js"/>
+///<reference path="~/Scripts/angular-mocks.js"/>
+///<reference path="~/Scripts/angular-local-storage.js"/>
+///<reference path="~/Scripts/loading-bar.js"/>
+///<reference path="~/Scripts/common.js"/>
+///<reference path="~/Scripts/App/Services/authInterceptorService.js"/>
+///<reference path="~/Scripts/App/Services/authService.js"/>
+///<reference path="~/Scripts/App/dataFactory.js"/>
+///<reference path="~/Scripts/App/app.js"/>
+///<reference path="~/Scripts/App/homeController.js"/>
+
+'use strict';
+describe("Route Config Spec", function () {
+
+    beforeEach(module("timeTrackerApp"));
+    
+    it('home route matches homeController',
+    inject(function ($route) {
+
+        expect($route.routes['/home'].controller).toBe('homeController');
+        expect($route.routes['/home'].templateUrl).toEqual('/views/Home.html');
+
+    }));
+
+    it('login route matches loginController',
+    inject(function ($route) {
+
+        expect($route.routes['/login'].controller).toBe('loginController');
+        expect($route.routes['/login'].templateUrl).toEqual('/views/Login.html');
+
+    }));
+
+    it('signup route matches loginController',
+    inject(function ($route) {
+
+        expect($route.routes['/signup'].controller).toBe('signupController');
+        expect($route.routes['/signup'].templateUrl).toEqual('/views/Signup.html');
+
+    }));
+
+    it('about route matches aboutController',
+    inject(function ($route) {
+
+        expect($route.routes['/about'].controller).toBe('aboutController');
+        expect($route.routes['/about'].templateUrl).toEqual('/views/About.html');
+
+    }));
+
+    it('customers route matches loginController',
+    inject(function ($route) {
+
+        expect($route.routes['/customers'].controller).toBe('customersController');
+        expect($route.routes['/customers'].templateUrl).toEqual('/views/Customers/CustomerList.html');
+
+    }));
+
+
+
+});
---
+++ TimeTracker.Web/Scripts/app/timeEntriesControllerSpec.js
@@ -0,0 +1,47 @@
+﻿///<reference path="~/Scripts/jasmine/jasmine.js"/>
+///<reference path="~/Scripts/angular.js"/>
+///<reference path="~/Scripts/angular-route.js"/>
+///<reference path="~/Scripts/angular-mocks.js"/>
+///<reference path="~/Scripts/angular-local-storage.js"/>
+///<reference path="~/Scripts/loading-bar.js"/>
+///<reference path="~/Scripts/common.js"/>
+///<reference path="~/Scripts/App/Services/authInterceptorService.js"/>
+///<reference path="~/Scripts/App/Services/authService.js"/>
+///<reference path="~/Scripts/App/app.js"/>
+///<reference path="~/Scripts/App/dataFactory.js"/>
+///<reference path="~/Scripts/App/timeEntriesController.js"/>
+
+describe("Time Entries Controller Spec", function () {
+    var $httpBackend, scope, ctrl, authRequestHandler;
+
+    beforeEach(module("timeTrackerApp"));
+
+    beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('/api/timeEntries')
+            .respond([{ "Id": 1, "ProjectId": 1, "Project": null, "UserId": "09d47e09-2c5e-4440-8582-a40b9da081c2", "User": null, "Description": "Entry1", "StartTime": "2014-12-08T12:00:00", "EndTime": "2014-12-08T17:00:00" }, { "Id": 2, "ProjectId": 3, "Project": null, "UserId": "6e37c971-09cb-4d39-904d-3fac94805a9a", "User": null, "Description": "Pølsevogn", "StartTime": "2014-12-08T08:00:00", "EndTime": "2014-12-08T11:00:00" }]);
+        scope = $rootScope.$new();
+        ctrl = $controller('timeEntriesController', { $scope: scope });
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should have title Time Entries ', inject(function () {
+        expect(scope.title).toBe('Time Entries');
+        $httpBackend.flush();
+        expect(scope.status).toBe('');
+
+    }));
+
+    it('should get a time entries list', inject(function () {
+        expect(scope.timeEntries).toEqual([]);
+        $httpBackend.flush();
+        expect(scope.timeEntries.length).toBe(2);
+        expect(scope.status).toBe('');
+
+    }));
+
+});
